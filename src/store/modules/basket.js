83d9ca583d488525f463d6b848e6b75953687585
Ruslan Melnyk <ruslan_melnyk@ukr.net>
2019-08-23 13:45:06 +0300

    basket.js fix

--- src/store/modules/basket.js
+++ src/store/modules/basket.js
@@ -1,17 +1,18 @@
 // Получение общего количества выбранных продуктов
-const getSum = (arr) => { return arr.reduce((sum, elem) => { return sum + elem.quantity }, 0) }
-
+const getSum = arr => arr.reduce((sum, elem) => sum + elem.quantity, 0)
 // Получение количества одного продукта
-const getQuantity = (arr, id) => {
-  const filterArr = arr.filter(elem => elem.product.id === id)
-  return getSum(filterArr)
-}
-
+const getQuantity = (arr, id) => getSum(arr.filter(elem => elem.product.id === id))
 // Изменение количества в выбраном продукте
-const changeQuantity = (arr, id, value) => {
-  return arr.forEach((elem, index) => {
-    if (elem.product.id === id) arr[index].quantity = value
+const changeQuantity = (arr, id, value) =>
+  arr.map(elem => (elem.product.id === id) ? { ...elem, quantity: value } : elem)
+// Получение коллекции уникальных продуктов
+const getUniqueProducts = arr => {
+  const collection = new Set()
+  const filter = val => arr.filter(item => {
+    let value = val(item)
+    return collection.has(value) ? false : collection.add(value)
   })
+  return filter(val => val.product.id)
 }
 
 export default {
@@ -66,19 +67,8 @@ export default {
       basket.sort((a, b) => new Date(b.date) - new Date(a.date))
       // Вычисления количества одного товара в корзине
       const quantity = getQuantity(basket, product.id)
-      // Коллекция уникальных продуктов
-      const collection = new Set()
-      // Добавление уникальных продуктов в коллекцию
-      const filter = (val) => basket.filter(item => {
-        let value = val(item)
-        return collection.has(value) ? false : collection.add(value)
-      })
-      // Отфильтрованный список продуктов
-      const filterBasket = filter(val => val.product.id)
-      // Изменеие количества одного продукта
-      filterBasket.forEach((elem, index) => {
-        if (elem.product.id === product.id) filterBasket[index].quantity = quantity
-      })
+      // Отфильтрованный список уникальных продуктов с изменением количества продуктов
+      const filterBasket = changeQuantity(getUniqueProducts(basket), product.id, quantity)
 
       commit('changeBasketList', filterBasket)
       dispatch('recount')
@@ -87,8 +77,8 @@ export default {
 
     changesQuantityProduct ({ state, commit, dispatch }, changes) {
       const basket = state.basket.basket_list
-      changeQuantity(basket, changes.id, changes.quantity)
-      commit('changeBasketList', basket)
+      const changeQuantityProduct = changeQuantity(basket, changes.id, changes.quantity)
+      commit('changeBasketList', changeQuantityProduct)
       dispatch('recount')
       localStorage.setItem('basket', JSON.stringify(state.basket))
     },
